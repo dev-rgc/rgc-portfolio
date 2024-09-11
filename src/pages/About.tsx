8f761dd7ae86b763940bcbed72ce179835c9326f
Rio Conge <riogarrell@gmail.com>
2024-09-11 22:37:08 +0800

    fixed initial layout and content for about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,13 @@
 import { motion } from "framer-motion";
+import Card from "../components/Card";
+import Dexp from "../data/work-experience";
+
 export default function About() {
   return (
-    /* CONTAINER */
-    <motion.div
-      className="h-full overflow-auto hide-scrollbar"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
+    <>
       {/* ABOUT */}
-      <div className="border-double border-4 border-[#3a86ff] rounded-lg p-3 text-[#adb5bd] shadow-md shadow-black">
-        <p className=" m-0">
+      <div className="border-double border-4 border-[#3a86ff] rounded-lg p-3 text-sm text-[#adb5bd] shadow-md shadow-black">
+        <p className="">
           I am a former SRE (Site Reliability Engineer) for a Cloud Engineering
           Team, but before that, I started as an intern. In my experience, I
           encountered many unfamiliar technologies, and it was a very fast-paced
@@ -45,141 +42,19 @@ export default function About() {
           you, and God bless!
         </p>
       </div>
-      {/* CONTAINER */}
-      <div className="m-0 p-0">
-        {/* EXPERIENCE-1 */}
-        {/* <motion.div
-          className="myCard mt-[1.5rem]"
-          initial={{ width: 0 }}
-          animate={{ width: "100%" }}
-          exit={{
-            x: window.innerWidth,
-            transition: { duration: "1500ms" },
-          }}
-        > */}
-        <h3>SRE</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          dignissim pharetra sapien non sodales. Praesent sapien nulla, cursus
-          et laoreet non, posuere quis lacus. Maecenas mattis nec velit vitae
-          iaculis. Sed dignissim posuere maximus. Mauris pellentesque, nisl vel
-          vehicula hendrerit, tellus urna eleifend nunc, vel facilisis turpis
-          massa id elit. Nam ornare ante vulputate, elementum massa eu, placerat
-          sapien. Fusce varius id dolor a aliquam. Integer ut erat auctor justo
-          vulputate tristique vitae vel felis. Donec id luctus velit, sit amet
-          feugiat lorem. Fusce ullamcorper egestas ullamcorper.
-        </p>
-        <ul>
-          <li>Git</li>
-          <li>Github</li>
-          <li>Cassandra</li>
-          <li>Kafka</li>
-          <li>Oracle</li>
-          <li>Splunk</li>
-          <li>Ansible</li>
-          <li>Teamcity</li>
-          <li>Linux</li>
-          <li>AWS</li>
-          <li>Grafana</li>
-          <li>Prometheus Alertmanager</li>
-          <li>DataStax OpsCenter</li>
-          <li>Rio</li>
-          <li>Mozart</li>
-          <li>Confluence</li>
-          <li>Slack</li>
-          <li>Communication</li>
-          <li>Teamwork</li>
-        </ul>
-        {/* </motion.div> */}
-        {/* EXPERIENCE-2 */}
-        {/* <motion.div
-          className="myCard mt-[1.5rem]"
-          initial={{ width: 0 }}
-          animate={{ width: "100%" }}
-          exit={{
-            x: window.innerWidth,
-            transition: { duration: 2 },
-          }}
-        > */}
-        <h3>Intern</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          dignissim pharetra sapien non sodales. Praesent sapien nulla, cursus
-          et laoreet non, posuere quis lacus. Maecenas mattis nec velit vitae
-          iaculis. Sed dignissim posuere maximus. Mauris pellentesque, nisl vel
-          vehicula hendrerit, tellus urna eleifend nunc, vel facilisis turpis
-          massa id elit. Nam ornare ante vulputate, elementum massa eu, placerat
-          sapien. Fusce varius id dolor a aliquam. Integer ut erat auctor justo
-          vulputate tristique vitae vel felis. Donec id luctus velit, sit amet
-          feugiat lorem. Fusce ullamcorper egestas ullamcorper.
-        </p>
-        <ul>
-          <li>Linux</li>
-          <li>VMware</li>
-          <li>Docker</li>
-          <li>Jenkins</li>
-          <li>JFrog</li>
-          <li>Java</li>
-          <li>Python</li>
-          <li>Git</li>
-          <li>Github</li>
-          <li>Bash script</li>
-          <li>Vim</li>
-          <li>Splunk</li>
-          <li>Cassandra</li>
-          <li>Kafka</li>
-          <li>Oracle</li>
-          <li>Hadoop</li>
-          <li>Apache Spark</li>
-          <li>Ansible</li>
-          <li>TeamCity</li>
-          <li>AWS</li>
-          <li>Kubernetes</li>
-          <li>Spinaker</li>
-          <li>Slack</li>
-          <li>Zoom</li>
-          <li>Communication</li>
-          <li>Teamwork</li>
-        </ul>
-        {/* </motion.div> */}
-        {/* EXPERIENCE-3 */}
-        {/* <motion.div
-          className="myCard mt-[1.5rem]"
-          initial={{ width: 0 }}
-          animate={{ width: "100%" }}
-          exit={{
-            x: window.innerWidth,
-            transition: { duration: 3 },
-          }} 
-        >*/}
-        <h3>Software Enginner</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          dignissim pharetra sapien non sodales. Praesent sapien nulla, cursus
-          et laoreet non, posuere quis lacus. Maecenas mattis nec velit vitae
-          iaculis. Sed dignissim posuere maximus. Mauris pellentesque, nisl vel
-          vehicula hendrerit, tellus urna eleifend nunc, vel facilisis turpis
-          massa id elit. Nam ornare ante vulputate, elementum massa eu, placerat
-          sapien. Fusce varius id dolor a aliquam. Integer ut erat auctor justo
-          vulputate tristique vitae vel felis. Donec id luctus velit, sit amet
-          feugiat lorem. Fusce ullamcorper egestas ullamcorper.
-        </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>PHP</li>
-          <li>Swift</li>
-          <li>My SQL</li>
-          <li>SQL lite</li>
-          <li>MS SQL</li>
-          <li>Git</li>
-          <li>Github</li>
-          <li>Communication</li>
-          <li>Teamwork</li>
-        </ul>
-        {/* </motion.div> */}
+
+      {/* SKILLS */}
+      <div className="m-0 p-0 ">
+        {Dexp.map((data) => (
+          <Card
+            id={data.id}
+            company={data.company}
+            title={data["job-title"]}
+            desc={data.description}
+            skills={data.skills}
+          />
+        ))}
       </div>
-    </motion.div>
+    </>
   );
 }
