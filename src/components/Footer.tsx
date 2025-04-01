aaef6363cdd90e7ee4b21225ec7be1dca89b6a1b
riogarrell <riogarrell@gmail.com>
2025-04-01 17:45:51 +0800

    modify github link to organization

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
   border-solid border-t-2 border-[#3a86ff] 
   text-[#3a86ff] "
     >
-      <a href="https://github.com/riogarrell" target="_blank">
+      <a href="https://github.com/dev-rgc" target="_blank">
         <FaGithub />
       </a>
       <a href="https://www.linkedin.com/in/riogarrell" target="_blank">
